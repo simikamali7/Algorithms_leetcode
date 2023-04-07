154ec64290bbad67157988fcfe2a059a2afcba15
Simi Kamali <kamalisimi7@gmail.com>
2023-04-06 20:07:07 -0600

    adding graph

--- python/graphs.py
+++ python/graphs.py
@@ -20,3 +20,33 @@
     # go wide first, then go deep
     # breadth-first search is used for finding shortest path (or just any path between 2 nodes)
     # breadth-first search is generally better than DFS
+def dfs(graph, source, visited, dfs_traversal):    
+    if source not in visited:
+        dfs_traversal.append(source)
+        visited.add(source)
+
+        for neighbor_node in graph[source]:
+            dfs(graph, neighbor_node, visited, dfs_traversal)
+
+    return dfs_traversal
+
+
+def main():
+    visited = set()
+    dfs_traversal = list()
+
+    graph = {
+        'A': ['B', 'C'],
+        'B': ['D', 'E'],
+        'C': ['F', 'G'],
+        'D': [],
+        'E': [],
+        'F': [],
+        'G': []
+    }
+
+    print(f"DFS: {dfs(graph, 'A', visited, dfs_traversal)}")
+
+
+if __name__=="__main__":
+    main()
