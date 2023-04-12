50e7d903e90fdc04e95aaea820252665d39fa727
Simi Kamali <kamalisimi7@gmail.com>
2023-04-11 20:43:20 -0600

    adding in top k frequent elements

--- python/leetcode_problems/top_k_frequent_elements.py
+++ python/leetcode_problems/top_k_frequent_elements.py
@@ -32,3 +32,60 @@ class Solution:
 
         num_vals = num_dict.values()
     
+
+
+# attempt 2:
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        # iterate through the list
+        for i in range(len(nums)):
+            current_num = i
+            current_count = 0 
+            if i+1 != current_num: 
+                return current_num
+            else: 
+                current_count += 1
+        # keep track of the number of each item 
+        # compare the sizes of the most frequent
+        # return
+
+
+# Neetcode solution 
+# use bucket sort
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # dictionary
+        count = {}
+
+        # make a list of lists, where the list is size of nums 
+        freq = [[] for i in range(len(nums)+ 1)]
+
+        # iterate through nums array, add in occurence into dictionary.
+        # dictionary keys = each unique number
+            # count.get(n, 0) --> grabs the value of the key "n", and if not exists, returns 0
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        
+        # for each key:value in count "count.items":
+            # go to that index in the frequency list of lists, and add n. 
+        for n, c in count.items():
+            freq[c].append(n)
+        
+
+        res = []
+    
+        # loop through the list of lists backwards, from last item to first. ((freq) - 1, 0, -1) --> means from the last index to 0, last argument is the step argument.
+        # for each index in the frequency list, add in that number into the results list to return.
+        # if the length of the results list == k --> stop the loop b/c found the top k element.
+
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+
+
+
+
