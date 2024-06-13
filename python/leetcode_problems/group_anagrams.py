02cf70ab9a7bb9c918c3f8abe17e6be55e1c1c8c
Simi Kamali <kamalisimi7@gmail.com>
2024-06-12 21:30:26 -0600

    group anagrams solution

--- python/leetcode_problems/group_anagrams.py
+++ python/leetcode_problems/group_anagrams.py
@@ -1,3 +1,4 @@
+# 49. Group Anagrams - Leetcode medium
 # Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
 # An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
@@ -18,25 +19,44 @@
 # Output: [["a"]]
 
 
-# First attempt
-# create a dictionary
-    # store each unique number as a key
-        # for value, store number of occurences of that key.
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
 
-
-
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
+        # create empty dictionary
         num_dict = {}
 
-        for i in range(len(nums)):
-                        
-            print(nums[i])
-            if nums[i] not in num_dict.keys():
-                num_dict[nums[i]] = 1
-            else:
-                num_dict[nums[i]] += 1
+        # loop through the array of strings 
+        for i in range(len(strs)):
+            
+            # sort each string, then join back together using join and sorted function.
+                # sort each string, using the sorted function, then join back together.
+            sorted_word = ''.join(sorted(strs[i]))
+            
+            
+            # check if the sorted string is a key within the dictionary
+                # if not, then create an empty list
+                # if does exist, just append that word into the list of that sorted corresponding word. 
+            if sorted_word not in num_dict:
+                num_dict[sorted_word] = []
+
+            num_dict[sorted_word].append(strs[i])
+
+
+        # after the finished iterating through the strs list
+            # get list of all the values(arrays) in num_dict dictionary, using num_dict.values()
+            # iterate through that list, and append it to the empty solution array that will be returned as the final solution array.
+        # create an empty array
+        sol_array = []
+        for array in num_dict.values():
+            sol_array.append(array)
+
+        return sol_array
+
+
 
     
 
